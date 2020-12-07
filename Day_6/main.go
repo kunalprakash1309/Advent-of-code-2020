a5935a866e8569e464127bd11f84348f3b272afa
kunal prakash <kunalprakash1309@gmail.com>
2020-12-07 23:14:41 +0530

    Day 6

--- Day_6/main.go
+++ Day_6/main.go
@@ -1,28 +1,90 @@
 package main
 
 import (
-	"strings"
-	"io/ioutil"
-	"fmt"
+  "fmt"
+  "strings"
+  "io/ioutil"
 )
 
-func getInput(filename string) ([]string, error){
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	return strings.Split(string(content), "\n\n"), nil
+func removeDuplicate(answer string) int {
+  s := ""
+  for _, v := range answer {
+    if strings.Contains(s, string(v)) {
+      
+    } else {
+      s = s+string(v)
+    }
+  }
+  return len(s)
 }
 
+func returnString(answer string) string {
+  s := ""
+  for _, v := range answer {
+    if strings.Contains(s, string(v)) {
+      
+    } else {
+      s = s+string(v)
+    }
+  }
+  return s
+}
+
+func remove(answer string) {
+  complete := ""
+  fixed := returnString(answer)
+  complete = complete + fixed
+}
+
+func secondPart(groups []string) {
+  total := 0
+
+  for _,v := range groups {
+    group := strings.Split(v, "\n")
+   
+   // for single element containg slice
+    if len(group) == 1 {
+      for _, y := range group {
+      sum := removeDuplicate(y)
+      total = total + sum
+      }
+    } else {
+      complete := ""
+      for _, y := range group {
+        fixed := returnString(y)
+        complete = complete + fixed
+      }
+      original := ""
+      for _, v := range complete{
+        count := strings.Count(complete, string(v))
+        if count == len(group){
+          original = original + string(v)
+        }
+      }
+      answer := removeDuplicate(original)
+      total = total + answer
+    }
+  }
+  fmt.Println("Second Part answer is := ", total)
+}
 
 func main() {
-	data, err := getInput("input.txt")
-	if err != nil {
-		fmt.Println("error in opening the file", err)
-	}
+  content, err := ioutil.ReadFile("input.txt")
+  if err != nil {
+    fmt.Println(err)
+  }
 
-	// fmt.Println(data)
-	fmt.Println("<<<<<<<<----->>>>>>>>")
-	fmt.Println(data[0])
+  var total int
+
+  groups := strings.Split(string(content), "\n\n")
+  for _,v := range groups {
+    group := strings.Split(v, "\n")
+    answer := strings.Join(group, "")
+
+    individual := removeDuplicate(answer)
+    total = total + individual
+  }
+  fmt.Println("First part answer is := ",total)
+
+  secondPart(groups)
 }
